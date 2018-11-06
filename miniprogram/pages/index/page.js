69a433bd3c3539409654313b8988b9a49b99cfbc
ChaZ <328103224@qq.com>
2018-11-06 13:38:20 +0800

    set jianlipage

--- miniprogram/pages/index/page.js
+++ miniprogram/pages/index/page.js
@@ -5,7 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-    current: 'info'
+    current: 'info', 
+    oneChecked: false,
+    tags: [
+      {
+        name: '标签一',
+        checked: false,
+        color: 'default'
+      },
+      {
+        name: '标签二',
+        checked: false,
+        color: 'red'
+      },
+      {
+        name: '标签三',
+        checked: false,
+        color: 'blue'
+      },
+      {
+        name: '标签4️',
+        checked: false,
+        color: 'green'
+      }
+    ],
+    skilltags: [
+      {
+        name: 'php(熟练)',
+        checked: true,
+        color: 'green'
+      },
+      {
+        name: 'JavaScript(一般)',
+        checked: true,
+        color: 'yellow'
+      },
+      {
+        name: 'html(精通)',
+        checked: true,
+        color: 'blue'
+      },
+      {
+        name: 'vue(了解)',
+        checked: true,
+        color: 'red'
+      }
+    ]
   },
 
   handleChange({ detail }) {
